698c3231a1773aebdc10054e01166395ebb69d73
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-17 21:26:44 +0530

    check no is even or odd

--- Program.cs
+++ Program.cs
@@ -6,29 +6,17 @@ namespace Basic_Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter any number :- ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            if (Num % 2 == 0)
             {
-                Console.WriteLine("Please Enter the number of times coin flip");
-                int number = Convert.ToInt32(Console.ReadLine());
-                Random random = new Random();
-                int heads = 0;
-                int tails = 0;
-                int temp = number;
-                while (number > 0)
-                {
-                    if (random.NextDouble() <= 0.5)
-                        heads++;
-                    else
-                        tails++;
-                    number--;
-
-                }
-                number = temp;
-                double percentageOfHead = heads * 100.0 / number;
-                double percentageOfTail = tails * 100.0 / number;
-                Console.WriteLine("Percentage of Head is " + percentageOfHead);
-                Console.WriteLine("Percentage of tail is " + percentageOfTail);
+                Console.WriteLine("Enter number is even");
             }
+            else
+            {
+                Console.WriteLine("Enter number is odd");
+            }
+            Console.ReadLine();
         }
     }
-}    
-
+}
