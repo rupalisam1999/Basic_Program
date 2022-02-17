fd16e54c19dbad9a1bdf527b9d5a0b67669e620b
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-17 11:20:43 +0530

    add project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
