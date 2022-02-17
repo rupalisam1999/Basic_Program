1ca1f72b895e8fb3768beffc7922aa4eee1ed757
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-17 22:10:25 +0530

    find largest no among three numbers

--- Program.cs
+++ Program.cs
@@ -9,24 +9,26 @@ namespace Basic_Program
           static void Main(string[] args)
         {
 
-            Console.Write("Enter a character to check wheather it is Vowel or Consonant : ");
-            char character = Convert.ToChar(Console.ReadLine().ToUpper());
-            switch (character)
-            {
-                case 'A':
-                case 'E':
-                case 'I':
-                case 'O':
-                case 'U':
-                    Console.WriteLine("Entered character is a Vowel");
-                    break;
-                default:
-                    Console.WriteLine("Entered character is a Consonant");
-                    break;
-            }
+            int first;
+            int second;
+            int third;
+
+            Console.WriteLine("Enter the first number : ");
+            first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number : ");
+            second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number : ");
+            third = int.Parse(Console.ReadLine());
+
+            int maxValue = Math.Max(Math.Max(first, second), third);
+
+            Console.WriteLine("Largest value : " + maxValue);
         }
     }
 }
+
         
 
     
